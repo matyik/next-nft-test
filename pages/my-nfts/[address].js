779e086c5d973f6d87e471a77a63a69fa51d9e59
Matyi Kari <matykari@gmail.com>
2021-07-27 12:41:29 -0600

    initial commit

---
+++ pages/my-nfts/[address].js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import axios from 'axios'
+
+const MyNFTs = ({ NFTs }) => {
+  return (
+    <>
+      <Head>
+        <title>My NFT's</title>
+      </Head>
+      <h1>My NFT's:</h1>
+      <div className='nft-container'>
+        {NFTs.result.map(({ hash, tokenID, tokenName, tokenSymbol }, index) => (
+          <span key={index} className='nft'>
+            {tokenName} #{tokenID}
+            <br />
+            {hash} ({tokenSymbol})
+          </span>
+        ))}
+        {NFTs.result.length === 0 && <span>No NFT's found.</span>}
+      </div>
+    </>
+  )
+}
+
+export const getServerSideProps = async (context) => {
+  const res = await axios.get(
+    `https://api-ropsten.etherscan.io/api?module=account&action=tokennfttx&address=${context.params.address}&startblock=0&endblock=999999999&sort=asc&apikey=${process.env.ETHERSCAN_KEY}`
+  )
+  const NFTs = await res.data
+
+  return {
+    props: {
+      NFTs
+    }
+  }
+}
+
+export default MyNFTs
---
+++ utils/pinata.js
@@ -0,0 +1,30 @@
+const key = process.env.ALCHEMY_KEY
+const secret = process.env.PINATA_SECRET
+
+const axios = require('axios')
+
+export const pinJSONToIPFS = async (JSONBody) => {
+  const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`
+  //making axios POST request to Pinata
+  return axios
+    .post(url, JSONBody, {
+      headers: {
+        pinata_api_key: '7d5183df54583ac91482',
+        pinata_secret_api_key:
+          'e1a9d836eada572af393312879c5d93094396919f2a9f9933d7c29e36bf3caa8'
+      }
+    })
+    .then(function (response) {
+      return {
+        success: true,
+        pinataUrl: 'https://gateway.pinata.cloud/ipfs/' + response.data.IpfsHash
+      }
+    })
+    .catch(function (error) {
+      console.log(error)
+      return {
+        success: false,
+        message: error.message
+      }
+    })
+}
